b9ffc0a106629c0622a820878b19fbffa060684d
Lencho2010 <335999737@qq.com>
2021-08-22 08:58:48 +0800

    删除多余模块

--- lencho-provider8001/src/main/java/com/geoway/webstore/config/MvcConfiguration.java
+++ lencho-provider8001/src/main/java/com/geoway/webstore/config/MvcConfiguration.java
@@ -2,6 +2,7 @@ package com.geoway.webstore.config;
 
 import com.geoway.webstore.interceptor.ResponseResultInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,15 @@ public class MvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ResponseResultInterceptor()).addPathPatterns("/**").order(1);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
     }
 }
