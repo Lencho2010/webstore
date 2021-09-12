f6c4e832f38cbb0fdeeeafc77966dbd65dee8e14
Lencho <335999737@qq.com>
2021-09-12 10:57:41 +0800

    集成log4j2日志插件

--- lencho-provider8001/src/main/java/com/geoway/webstore/controller/ZipFileInfoController.java
+++ lencho-provider8001/src/main/java/com/geoway/webstore/controller/ZipFileInfoController.java
@@ -4,7 +4,10 @@ import com.geoway.webstore.anno.ResponseResult;
 import com.geoway.webstore.dto.ZipFileInfoDto;
 import com.geoway.webstore.entities.ZipFileInfo;
 import com.geoway.webstore.service.ZipFileInfoService;
+import com.geoway.webstore.service.impl.ZipFileInfoServiceImpl;
 import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -18,6 +21,9 @@ import java.util.List;
 @RequestMapping("/zipFileInfo")
 @ResponseResult
 public class ZipFileInfoController {
+
+    private static final Logger log = LoggerFactory.getLogger(ZipFileInfoController.class);
+
 
     @Resource
     private ZipFileInfoService zipFileInfoService;
@@ -49,14 +55,15 @@ public class ZipFileInfoController {
 
     @GetMapping(value = {"/listByStatus/{page}/{rows}"})
     public PageInfo listByStatus(@PathVariable("page") int page,
-                                  @PathVariable("rows") int rows,
-                                  @RequestParam(value = "taskName", required = false) String taskName,
-                                  @RequestParam(value = "status", required = false) List<Integer> status) {
+                                 @PathVariable("rows") int rows,
+                                 @RequestParam(value = "taskName", required = false) String taskName,
+                                 @RequestParam(value = "status", required = false) List<Integer> status) {
+        log.info("listByStatus ### page:" + page + ";rows:" + rows);
         return zipFileInfoService.listByStatus(page, rows, taskName, status);
     }
 
     @PutMapping("/updateStatus")
-    public boolean updateStatus(@RequestBody ZipFileInfoDto zipFileInfo){
+    public boolean updateStatus(@RequestBody ZipFileInfoDto zipFileInfo) {
         return zipFileInfoService.updateStatus(zipFileInfo);
     }
 }
