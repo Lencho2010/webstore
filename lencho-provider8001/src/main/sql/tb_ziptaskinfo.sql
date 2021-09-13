0a3d91ec51eaca7ce4d710beb80c84187a582b20
Lencho <335999737@qq.com>
2021-09-13 20:33:44 +0800

    数据库结构提交

--- lencho-provider8001/src/main/sql/tb_ziptaskinfo.sql
+++ lencho-provider8001/src/main/sql/tb_ziptaskinfo.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : postgresql@docker
+ Source Server         : postgres@docker-hadoop104
  Source Server Type    : PostgreSQL
  Source Server Version : 120001
  Source Host           : hadoop104:5432
@@ -12,7 +12,7 @@
  Target Server Version : 120001
  File Encoding         : 65001
 
- Date: 07/09/2021 11:36:58
+ Date: 13/09/2021 20:32:17
 */
 
 
@@ -26,12 +26,22 @@ CREATE TABLE "public"."tb_ziptaskinfo" (
   "f_name" varchar(255) COLLATE "pg_catalog"."default",
   "f_status" numeric(255),
   "f_progress" numeric(255),
-  "f_starttime" date,
-  "f_endtime" date
+  "f_starttime" timestamp(6),
+  "f_endtime" timestamp(6)
 )
 ;
 COMMENT ON COLUMN "public"."tb_ziptaskinfo"."f_code" IS '关联任务主键';
 COMMENT ON COLUMN "public"."tb_ziptaskinfo"."f_name" IS '步骤名称';
+
+-- ----------------------------
+-- Records of tb_ziptaskinfo
+-- ----------------------------
+INSERT INTO "public"."tb_ziptaskinfo" VALUES (1001, '2021S205190028', '接收解压', 1, 100, '2021-09-06 15:20:39', '2021-09-08 15:20:43');
+INSERT INTO "public"."tb_ziptaskinfo" VALUES (1002, '2021S205190028', '质量检查', 1, 100, '2021-09-07 15:21:21', '2021-09-08 15:21:27');
+INSERT INTO "public"."tb_ziptaskinfo" VALUES (1006, '2021S205190028', '数据入库', 1, 0, '2021-09-07 15:23:28', '2021-09-10 15:23:34');
+INSERT INTO "public"."tb_ziptaskinfo" VALUES (1004, '2021S205190028', '成果检查', 1, 60, '2021-09-07 15:22:30', '2021-09-08 15:22:34');
+INSERT INTO "public"."tb_ziptaskinfo" VALUES (1003, '2021S205190028', '套合比对', 1, 80, '2021-09-07 15:21:59', '2021-09-09 15:22:02');
+INSERT INTO "public"."tb_ziptaskinfo" VALUES (1005, '2021S205190028', '成果输出', 1, 40, '2021-09-06 15:22:55', '2021-09-08 15:22:59');
 
 -- ----------------------------
 -- Primary Key structure for table tb_ziptaskinfo
--- lencho-provider8001/src/main/sql/tbsys_syspara.sql
+++ lencho-provider8001/src/main/sql/tbsys_syspara.sql
@@ -1,7 +1,7 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : postgresql@docker
+ Source Server         : postgres@docker-hadoop104
  Source Server Type    : PostgreSQL
  Source Server Version : 120001
  Source Host           : hadoop104:5432
@@ -12,7 +12,7 @@
  Target Server Version : 120001
  File Encoding         : 65001
 
- Date: 07/09/2021 11:37:49
+ Date: 13/09/2021 20:32:32
 */
 
 
@@ -32,6 +32,18 @@ CREATE TABLE "public"."tbsys_syspara" (
   "f_type" numeric(255)
 )
 ;
+
+-- ----------------------------
+-- Records of tbsys_syspara
+-- ----------------------------
+INSERT INTO "public"."tbsys_syspara" VALUES (1004, 'scanPaths', 'a10519d2-85f3-4b06-b38a-2df9c6462623', '[
+"hadoop104:5432/db2021",
+"hadoop103:5432/db2021",
+"hadoop102:5432/db2021"
+]', NULL, '扫描路径备选', NULL, NULL, NULL);
+INSERT INTO "public"."tbsys_syspara" VALUES (1003, 'scanPath', 'eefc68d6-50d0-46ee-ae60-f7556941e92c', 'hadoop104:5432/db2021', NULL, '扫描路径', NULL, NULL, NULL);
+INSERT INTO "public"."tbsys_syspara" VALUES (1002, 'scanInterval', 'aadea8a9-b7ad-44e2-a7a4-6d37ae432742', '25', NULL, '扫描间隔：单位分钟', NULL, NULL, NULL);
+INSERT INTO "public"."tbsys_syspara" VALUES (1001, 'serviceStatus', 'add77e71-6603-4dd7-9ff9-6e2a947c398e', '1', NULL, '服务状态：0-暂停；1-启动', NULL, NULL, NULL);
 
 -- ----------------------------
 -- Primary Key structure for table tbsys_syspara
