bc797759c2f3c4cf6e06e032cb8731308d5ad646
Lencho <335999737@qq.com>
2021-09-14 22:18:54 +0800

    修改警告

--- lencho-rocketmq/src/main/java/com/geoway/webstore/controller/MqMessageController.java
+++ lencho-rocketmq/src/main/java/com/geoway/webstore/controller/MqMessageController.java
@@ -2,7 +2,6 @@ package com.geoway.webstore.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.spring.core.RocketMQTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
